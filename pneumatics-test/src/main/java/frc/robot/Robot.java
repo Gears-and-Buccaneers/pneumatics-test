edb9110514862eb7c3e5cef0fe2a33c1d81c2c77
Jonah Lefkoff <jonah_lefkoff@hotmail.com>
2018-12-26 16:09:00 -0700

    deleted unecessary imports

--- pneumatics-test/src/main/java/frc/robot/Robot.java
+++ pneumatics-test/src/main/java/frc/robot/Robot.java
@@ -10,8 +10,6 @@ import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 
 public class Robot extends IterativeRobot {
